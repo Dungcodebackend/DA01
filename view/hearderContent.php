f7b51bab0cb6c91c1b646b9f022ec6ece5e027a0
Dungcodebackend <dungbqph46321@fpt.edu.vn>
2023-08-11 22:08:17 +0700

    file new

---
+++ view/hearderContent.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="/fontawesome-free-6.4.2-web/css/all.min.css" />
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <div class="img__logo">
+        <img src="" alt="" id="img" />
+    </div>
+    <div style="display: flex; align-items: center; padding-right: 20px">
+        <div id="avarta"></div>
+        <a href=""><span>Đăng Xuất</span></a>
+    </div>
+</header>
+<div class="Content">
+    <nav>
+        <h3>MENU</h3>
+        <div class="nav__box">
+            <a href="">
+                <span>Danh Sách Thành Viên </span>
+                <i class="fa-solid fa-angles-right"></i>
+            </a>
+        </div>
+        <div class="nav__box">
+            <a href="">
+                <span>Cập Nhật Thông Tin </span>
+                <i class="fa-solid fa-angles-right"></i>
+            </a>
+        </div>
+    </nav>
